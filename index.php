672a1b1480211796089914a8c563c5d7f74ff07c
minnypatel <minnypatel@googlemail.com>
2018-09-24 23:38:14 +0100

    add privacy policy modal

--- index.php
+++ index.php
@@ -90,7 +90,8 @@
 
   <?php if(!isset($_COOKIE["comply_cookie"])) { ?>
     <div id="cookies";>
-      <p>Our website uses cookies. By continuing we assume your permission to deploy cookies, as detailed in our <a href="yourPolicy">privacy policy</a>.
+      <p>Our website uses cookies. By continuing we assume your permission to deploy cookies, as detailed in our
+        <a onclick="modalOpen('privacy-policy-modal')">privacy policy</a>.
         <button onclick="cookieBannerClose()">Close</button>
       </p>
     </div>
@@ -439,10 +440,12 @@
 
 <!-- Footer -->
 <footer class="w3-center w3-light-grey w3-padding-48 w3-large">
-  <button class="modal-button" onclick="modalOpen()"><?=$credits_txt?></button>
+  <button class="modal-button" onclick="modalOpen('credits-modal')"><?=$credits_txt?></button>
+  <button class="modal-button" onclick="modalOpen('privacy-policy-modal')">Privacy Policy</button>
+
   <div id="credits-modal" class="modal">
     <div class="modal-content">
-      <span class="modal-close" onclick="modalClose()">&times;</span>
+      <span class="modal-close" onclick="modalClose('credits-modal')">&times;</span>
       <h3>Project Co-ordinators:</h3>
       <p>Chris Harrison; Fabrizio Arrigoni Battaia; Lucy Moorcraft</p>
       <h3>Website:</h3>
@@ -452,6 +455,36 @@
       <h3>Tremendous support from:</h3>
       <p>Mylene Andre, Stella-Maria Chasiotis-Klingner, Anne-Laure Cheffot, Tania Johnston, Mariya Lyubenova, Juliette Ortet, Nicole Shearer, Nelma Silva, Alasdair Thomson, Wolfgang Vieser, Sebastian Wassill, Saskia, Elizabeth, Alex</p>
       <h3>Financial Support from ESO SSDF</h3>
+    </div>
+  </div>
+
+  <div id="privacy-policy-modal" class="modal">
+    <div class="modal-content">
+      <span class="modal-close" onclick="modalClose('privacy-policy-modal')">&times;</span>
+      <h3>Privacy Policy</h3>
+      <p>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from http://biggesteyeonthesky.org/ (the "Site").</p>
+      <h3>PERSONAL INFORMATION WE COLLECT</h3>
+      <p>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information."</p>
+      <p>We collect Device Information using the following technologies:</p>
+      <ul>
+          <li>"Cookies" are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.</li>
+          <li>"Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</li>
+          <li>"Web beacons," "tags," and "pixels" are electronic files used to record information about how you browse the Site.</li>
+      </ul>
+      <p>When we talk about "Personal Information" in this Privacy Policy, we are talking both about Device Information.</p>
+      <h3>HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>
+      <p>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our Users browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).</p>
+      <h3>SHARING YOUR PERSONAL INFORMATION</h3>
+      <p>We share your Personal Information with third parties to help us use your Personal Information, as described above.  For example, we use Google Analytics to help us understand how our Users use the Site--you can read more about how Google uses your Personal Information here:  https://www.google.com/intl/en/policies/privacy/.  You can also opt-out of Google Analytics here:  https://tools.google.com/dlpage/gaoptout.</p>
+      <p>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</p>
+      <h3>DO NOT TRACK</h3>
+      <p>Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.</p>
+      <h3>YOUR RIGHTS</h3>
+      <p>If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.</p>
+      <h3>CHANGES</h3>
+      <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+      <h3>CONTACT US</h3>
+      <p>For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at <strong>contact@biggesteyeonthesky.org</strong></p>
     </div>
   </div>
 
