afe02107ace241129cbe36488552cd59f0d091e6
cmharrison-astro <c.m.harrison@mail.com>
2018-09-25 21:25:46 +0200

    French updated

--- languages/lang_fr.php
+++ languages/lang_fr.php
@@ -1,15 +1,15 @@
 <?php
 
-  $about_txt='Sur';
+  $about_txt='A propos';
   $resources_txt='Ressources';
   $exoplanets_txt='Exoplanètes';
-  $foreveryone_txt='Pour tout le monde';
+  $foreveryone_txt='Pour tout le grand public';
   $foreducators_txt='Pour les éducateurs';
-  $explorePlanets_txt='EXPLOREZ VOS<br/>EXPLANETS';
+  $explorePlanets_txt='EXPLOREZ VOS<br/>EXOPLANETES';
   $overview_txt="Vue d'ensemble";
-  $yourpics_txt="Vos photos!";
+  $yourpics_txt="Vos photos !";
   $otherpics_txt="Autres photos";
   $select_star_txt="Étoile ";
-  $selected_star_txt="Étoile sélectionné: ";
+  $selected_star_txt="Étoile sélectionnée: ";
   $credits_txt="Crédits";
 ?>
